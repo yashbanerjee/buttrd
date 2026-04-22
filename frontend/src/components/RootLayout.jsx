c11b6177bc71305a6fb87cf53da9202f80ceca4e
B Yash <yashbanerjee07@gmail.com>
2026-04-22 16:26:06 +0400

    fixed the new page loading bug

--- frontend/src/components/RootLayout.jsx
+++ frontend/src/components/RootLayout.jsx
@@ -1,12 +1,17 @@
-import { useEffect, useState } from 'react'
-import { Outlet } from 'react-router-dom'
+import { useEffect, useLayoutEffect, useState } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import { FirstVisitSplash, shouldShowFirstVisitSplash } from './FirstVisitSplash.jsx'
 import { MobileNav } from './MobileNav.jsx'
 import { SiteNav } from './SiteNav.jsx'
 
 export function RootLayout() {
+  const { pathname } = useLocation()
   const [mobileOpen, setMobileOpen] = useState(false)
   const [showSplash, setShowSplash] = useState(shouldShowFirstVisitSplash)
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+  }, [pathname])
 
   useEffect(() => {
     document.body.style.overflow = mobileOpen || showSplash ? 'hidden' : ''
